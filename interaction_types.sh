cfd4d5626dcfd78aea396706a7f105896fa52fc9
amch-med23 <skyied.med2023@gmail.com>
2024-12-16 11:09:28 +0100

    addiing the insertion script for interactions_types.json file

---
+++ interaction_types.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Read JSON file and parse it to insert into MySQL adverse_effects table
+cat interaction_types.json | jq -c '.[]' | while read interaction_type; do
+    id=$(echo $interaction_type | jq -r '.id')
+    name=$(echo $interaction_type | jq -r '.name' | sed "s/'/''/g")
+    category=$(echo $interaction_type | jq -r '.category' | sed "s/'/''/g")
+    description=$(echo $interaction_type | jq -r '.description' | sed "s/'/''/g")
+
+    # Check if the record with the same id exists and insert it if it doesn't
+    mysql -u root -p 'rootP@ssw0rd' -e "
+    USE apotek_db;
+    SET NAMES 'utf8mb4';
+    INSERT IGNORE INTO interaction_types (id, name, category, description)
+    VALUES ($id, '$name', '$category', $description);"
+done
