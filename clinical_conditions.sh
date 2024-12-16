50abbbe696d5e0472d8d434bb98ebd9d8b14c463
amch-med23 <skyied.med2023@gmail.com>
2024-12-16 10:51:57 +0100

    adding clinical_conditions insertion script

---
+++ clinical_conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Read JSON file and parse it to insert into MySQL clinical_conditions table
+cat clinical_conditions.json | jq -c '.[]' | while read condition; do
+    id=$(echo $condition | jq -r '.id')
+    name=$(echo $condition | jq -r '.name' | sed "s/'/''/g")
+    severity=$(echo $condition | jq -r '.severity' | sed "s/'/''/g")
+    description=$(echo $condition | jq -r '.description' | sed "s/'/''/g")
+
+    # Check if the record with the same id exists and insert it if it doesn't
+    mysql -u root -p 'rootP@ssw0rd' -e "
+    USE apotek_db;
+    SET NAMES 'utf8mb4';
+    INSERT IGNORE INTO clinical_conditions (id, name, severity, description)
+    VALUES ($id, '$name', '$severity', $description);"
+done
