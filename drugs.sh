04719dde4f2f7e6799fe0ed16499aff36becfd79
amch-med23 <skyied.med2023@gmail.com>
2024-12-16 07:14:06 +0100

    using INSERT IGNORE statement rather than the IF Statement used earlier, which caused problems

--- drugs.sh
+++ drugs.sh
@@ -10,10 +10,8 @@ cat drugs.json | jq -c '.[]' | while read drug; do
     prescription_required=$(echo $drug | jq -r '.prescription_required')
 
     # Check if the record with the same id exists and insert it if it doesn't
-    mysql -u root -p '' -e "
+    mysql -u root -p -e "
     USE apotek_db;
-    IF NOT EXISTS (SELECT 1 FROM drugs WHERE id = $id) THEN
-        INSERT INTO drugs (id, name, generic_name, brand_name, dosage_form, route_admin, prescription_required)
-        VALUES ($id, '$name', '$generic_name', '$brand_name', '$dosage_form', '$route_admin', $prescription_required);
-    END IF;"
+    INSERT IGNORE INTO drugs (id, name, generic_name, brand_name, dosage_form, route_admin, prescription_required)
+    VALUES ($id, '$name', '$generic_name', '$brand_name', '$dosage_form', '$route_admin', $prescription_required);"
 done
